52cb13659d90e1f7820fcb2b373f2e74c97870a3
sakthi2105 <sakthigopi210505@gmail.com>
2026-04-25 17:25:39 +0530

    went

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ const getShopsRoute = require("./routes/getShops");
 
 const app = express();
 
-app.use(cors());
+const cors = require("cors");
+
+app.use(cors({
+  origin: "https://app.netlify.com/projects/menswear-coderead2026/"
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json());
